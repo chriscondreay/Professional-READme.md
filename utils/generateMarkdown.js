603204fa660f1574564069034da312565e2a9e66
ZooWee <condreaychris@gmail.com>
2021-10-02 10:57:10 -0700

    more change in file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const mitLicense = "MIT";
+  const apacheLicense = "Apache License 2.0";
+  const unlicense = "The Unlicense"
+  switch (license) {
+    case mitLicense:
+      return `[!License](https://img.shields.io/badge/License-MIT-blue.svg)]`;
+      break;
+    case apacheLicense:
+      return `[!License](https://img.shields.io/badge/License-Apache%202.0-green.svg)]`;
+      break;
+    case unlicense:
+      return `[!License](https://img.shields.io/badge/License-Unlicense-red.svg)]`
+    default:
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
